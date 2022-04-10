f965487fa21e15a103370f5dcfa2f0d9475ac245
Brian Huynh <80880024+bhuynh987@users.noreply.github.com>
2022-04-10 06:45:19 -0700

    Update answerController.java
    Changed required imports to match

--- src/answerController.java
+++ src/answerController.java
@@ -1,9 +1,5 @@
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
